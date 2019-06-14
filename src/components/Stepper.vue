d247d5429c5d709cc26654f9b4d635347dc65382
Victor Chubirka <viktor.chubirka@transoftgroup.com>
2019-06-14 18:41:38 +0300

    add prev/next in stepper

--- src/components/Stepper.vue
+++ src/components/Stepper.vue
@@ -2,7 +2,7 @@
   <v-stepper :value="activeStep">
     <v-stepper-header>
       <div v-for="(item, n) in header" :key="`${n}-stepper`">
-        <v-stepper-step :complete="completeStep > n+1" :step="n+1">
+        <v-stepper-step :complete="completeStep > n" :step="n+1">
           <v-tooltip bottom>
             <template v-slot:activator="{ on }">
               <div flat color="primary" dark v-on="on">{{item.title}}</div>
@@ -15,14 +15,15 @@
     </v-stepper-header>
 
     <v-stepper-items>
-      <v-stepper-content v-for="(item, n) in header" :key="`${n}-content`" :step="n+1">
+      <v-stepper-content v-for="(item, n) in header" :key="`${n}-content`" :step="n">
 
         <div class="sttepper" v-if="n===0">
           <v-card  class="mb-5" color="grey lighten-1" height="200px">
             <p>Для продовження вам потрыбно пройти повну режстрацію в настройках профіля</p>
             <v-btn color="main">Account Settings</v-btn>
           </v-card>
-          <v-btn color="primary" @click="nextStep(n+1)">Continue</v-btn>
+          <!-- <v-btn color="primary" @click="nextStep(n+1)">Continue</v-btn>
+          <v-btn color="primary" @click="prevStep(n-1)" :disabled="canPrev">prev</v-btn> -->
         </div>
         
         <div class="sttepper" v-if="n===1">
@@ -33,7 +34,8 @@
             <input type="file">
             <input type="file">
           </v-card>
-          <v-btn color="primary" @click="nextStep(n+1)">Continue</v-btn>
+          <!-- <v-btn color="primary" @click="nextStep(n+1)">Continue</v-btn>
+          <v-btn color="primary" @click="prevStep(n-1)" :disabled="canPrev">prev</v-btn> -->
         </div>
 
         <div class="sttepper" v-if="n===2">
@@ -41,15 +43,17 @@
             <p>Вам потрібно оплатити</p>
             <v-btn color="main">Pay</v-btn>
           </v-card>
-          <v-btn color="primary" @click="nextStep(n+1)">Continue</v-btn>
+          <!-- <v-btn color="primary" @click="nextStep(n+1)">Continue</v-btn>
+          <v-btn color="primary" @click="prevStep(n-1)" :disabled="canPrev">prev</v-btn> -->
         </div>
         <div class="sttepper" v-if="n===3">
           <v-card  class="mb-5" color="grey lighten-1" height="200px">
             <p>Дякую за все чекайте результатыв на пошту</p>
             <v-btn color="main">Pay</v-btn>
           </v-card>
-          <v-btn color="primary" @click="nextStep(n+1)">Continue</v-btn>
         </div>
+          <v-btn color="primary" @click="nextStep(n)" :disabled="canNext">Continue</v-btn>
+          <v-btn color="primary" @click="prevStep(n)" :disabled="canPrev">prev</v-btn>
       </v-stepper-content>
     </v-stepper-items>
   </v-stepper>
@@ -91,15 +95,28 @@ export default {
     }),
     completeStep(){
       return this.activeStep;
+    },
+    canNext(){
+      return this.activeStep >= this.header.length - 1;
+    },
+    canPrev(){
+      return this.activeStep < 1;
     }
   },
 
   methods: {
     nextStep(n) {
-      if (n === this.steps) {
-        this.activeStep = 1;
+      if (n === this.header.length) {
+        this.activeStep = this.header.length;
       } else {
         this.activeStep = n + 1;
+      }
+    },
+    prevStep(n) {
+      if (n === 0) {
+        this.activeStep = 1;
+      } else {
+        this.activeStep = n - 1;
       }
     },
 
@@ -112,7 +129,7 @@ export default {
 
     mounted(){
       if(this.profile.accountPlus){
-        this.activeStep = 2;
+        this.activeStep = 0;
       }
     }
 
